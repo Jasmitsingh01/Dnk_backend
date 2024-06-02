1468acc76d627b9f8579e76a17efca0fe579f4cc
Jasmitsingh01 <jasmits007@gmail.com>
2024-06-02 20:42:50 +0530

    fix Changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://dnkecommerce.netlify.app",
+    origin: process.env.Dev_enviorment?"http://localhost:5173":"https://dnkecommerce.netlify.app",
     credentials: true,
     optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
