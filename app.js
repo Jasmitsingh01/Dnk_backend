ddecaf91f0d6d93e5a6214c5d1850f15f0531428
Jasmitsingh01 <97171634+Jasmitsingh01@users.noreply.github.com>
2024-11-22 23:53:57 +0530

    origin update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"https://dnkecommerce.netlify.app",
+    origin:"*",
     credentials: true,
     optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
